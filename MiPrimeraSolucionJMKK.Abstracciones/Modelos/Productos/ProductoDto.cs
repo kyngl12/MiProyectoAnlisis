b9a8d10c4ac43ea664239b8d227546b7dffc7b55
Kevin Yang <kevinyangli123@gmail.com>
2026-06-04 14:17:44 -0600

    Cambios Finales

--- MiPrimeraSolucionJMKK.Abstracciones/Modelos/Productos/ProductoDto.cs
+++ MiPrimeraSolucionJMKK.Abstracciones/Modelos/Productos/ProductoDto.cs
@@ -4,38 +4,22 @@ namespace MiPrimeraSolucionJMKK.Abstracciones.Modelos.Productos
 {
     public class ProductoDto
     {
-        [Display(Name = "Codigo")]
         public string Codigo { get; set; }
 
-        [(ErrorMessage = "El nombre es obligatorio")]
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
         [Display(Name = "Nombre")]
         public string Nombre { get; set; }
 
-        [Required(ErrorMessage = "La categoria es obligatoria")]
-        [Display(Name = "Categoria")]
+        [Required(ErrorMessage = "La categoría es obligatoria.")]
+        [Display(Name = "Categoría")]
         public string Categoria { get; set; }
 
-        [Range(0, 9999999999.0, ErrorMessage = "La cantidad debe ser mayor o igual a 0")]
+        [Range(0, 9999999999.0, ErrorMessage = "La cantidad debe ser mayor o igual a 0.")]
         [Display(Name = "Cantidad")]
         public decimal Cantidad { get; set; }
 
-        [Range(0, 9999999999.0, ErrorMessage = "El precio debe ser mayor o igual a 0")]
-        [Display(Name = "PrecioUnitario")]
+        [Range(0, 9999999999.0, ErrorMessage = "El precio debe ser mayor o igual a 0.")]
+        [Display(Name = "Precio Unitario")]
         public decimal PrecioUnitario { get; set; }
     }
 }
-
-/*
-
-{
-
-        public string Codigo { get; set; }
-
-    [Required(ErrorMessage = "El nombre es obligatorio.")]
-    public string Nombre { get; set; }
-    public string Categoria { get; set; }
-    public decimal Cantidad { get; set; }
-    public decimal PrecioUnitario { get; set; }
-    }
-}
-*/
