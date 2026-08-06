727ffe2d0766bb093b3e080b977118dad085c058
Kevin Yang <kevinyangli123@gmail.com>
2026-08-06 04:36:37 -0600

    Parte Mary Interfaz Gestion

--- MiPrimeraSolucionJMKK.AccesoADatos/Ordenes/ObtenerOrdenesAD.cs
+++ MiPrimeraSolucionJMKK.AccesoADatos/Ordenes/ObtenerOrdenesAD.cs
@@ -16,20 +16,32 @@ namespace GestionPubRock.AccesoADatos.Ordenes
         public List<MiPrimeraSolucionJMKK.Abstracciones.Modelos.Ordenes.OrdenListDto> ObtenerTodos()
         {
             string sql = @"
-                SELECT o.ID_ORDEN AS IdOrden, o.FECHA_ORDEN AS FechaOrden, o.OBSERVACIONES AS Observaciones,
-                       (SELECT COUNT(1) FROM PUBROCK_DETALLE_ORDEN_TB d WHERE d.ID_ORDEN = o.ID_ORDEN) AS CantidadItems,
-                       o.ID_ESTADO_ORDEN AS IdEstadoOrden, eo.DESCRIPCION AS EstadoOrden
-                FROM PUBROCK_ORDEN_TB o
-                LEFT JOIN PUBROCK_ESTADO_ORDEN_TB eo ON o.ID_ESTADO_ORDEN = eo.ID_ESTADO_ORDEN
-                WHERE o.ID_ESTADO = (SELECT ID_ESTADO FROM PUBROCK_ESTADO_TB WHERE DESCRIPCION = 'Activo')
-                ORDER BY o.ID_ORDEN DESC
-            ";
+    SELECT
+        o.ID_ORDEN AS IdOrden,
+        o.FECHA_ORDEN AS FechaOrden,
+        o.OBSERVACIONES AS Observaciones,
+        (
+            SELECT COUNT(1)
+            FROM PUBROCK_DETALLE_ORDEN_TB d
+            WHERE d.ID_ORDEN = o.ID_ORDEN
+        ) AS CantidadItems,
+        o.ID_ESTADO_ORDEN AS IdEstadoOrden,
+        eo.DESCRIPCION AS EstadoOrdenDescripcion
+    FROM PUBROCK_ORDEN_TB o
+    LEFT JOIN PUBROCK_ESTADO_ORDEN_TB eo
+        ON o.ID_ESTADO_ORDEN = eo.ID_ESTADO_ORDEN
+    WHERE o.ID_ESTADO = (
+        SELECT TOP 1 ID_ESTADO
+        FROM PUBROCK_ESTADO_TB
+        WHERE DESCRIPCION = 'Activo'
+        ORDER BY ID_ESTADO
+    )
+    ORDER BY o.ID_ORDEN DESC
+";
 
-            var raw = _ctx.Database.SqlQuery<MiPrimeraSolucionJMKK.Abstracciones.Modelos.Ordenes.OrdenListDto>(sql);
-            var list = new List<MiPrimeraSolucionJMKK.Abstracciones.Modelos.Ordenes.OrdenListDto>();
-            foreach (var item in raw)
-                list.Add(item);
-            return list;
+            return _ctx.Database
+    .SqlQuery<MiPrimeraSolucionJMKK.Abstracciones.Modelos.Ordenes.OrdenListDto>(sql)
+    .ToList();
         }
     }
 }
